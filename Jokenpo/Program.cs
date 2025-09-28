6d288383bf4fd2f1224626e2b51057a21532d214
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-09-28 08:44:40 -0300

    first commit

---
+++ Jokenpo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Jokenpo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Jogar(new Pedra(), new Tesoura()));
+        }
+
+        public static string Jogar(Movimento jogador, Movimento adversario)
+        {
+            if (jogador.Nome == adversario.Nome) return "Empate";
+            return jogador.GanhaDe(adversario) ? "Vitória" : "Derrota";
+        }
+
+        public abstract class Movimento
+        {
+            public abstract string Nome { get; }
+            public abstract bool GanhaDe(Movimento outro);
+        }
+
+        public class Pedra : Movimento
+        {
+            public override string Nome => "Pedra";
+            public override bool GanhaDe(Movimento outro) => outro is Tesoura;
+        }
+
+        public class Papel : Movimento
+        {
+            public override string Nome => "Papel";
+            public override bool GanhaDe(Movimento outro) => outro is Pedra;
+        }
+
+        public class Tesoura : Movimento
+        {
+            public override string Nome => "Tesoura";
+            public override bool GanhaDe(Movimento outro) => outro is Papel;
+        }
+    }
+}
